fb53aab982844a7b5a28036b39ca3042583fc4b7
Aubrey Wullschleger <aubrey.wullschleger@gmail.com>
2015-08-20 14:27:33 -0600

    first draft

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 (function () {
     var app = angular.module('redditClone', []);
 
-    app.controller()
-});
+    app.controller('postController', function ($scope) {
+        $scope.posts = [];
+
+        $scope.addPost = function () {
+            $scope.posts.push({
+                title: $scope.post.title,
+                author: $scope.post.author,
+                image: $scope.post.image,
+                description: $scope.post.description,
+                date: $scope.post.date = new Date()
+            });
+        };
+
+        $scope.castVote = {
+            upVotes: 0,
+            downVotes: 0,
+            totalVotes: $scope.upVotes + $scope.downVotes
+        };
+
+        $scope.orderVotes = function() {
+            $scope.posts.push({votes: $scope.castVote.totalVotes});
+            console.log($scope.posts);
+        };
+        //
+        //$scope.orderVotes = function (arr) {
+        //    for(i=0; i<arr.length; i++) {
+        //     if(arr[i].)
+        //    }
+        //}
+    });
+})();
